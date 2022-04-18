3350a4629dc715f3489c7c13b1ede01a0c8636c7
Krystian Nowakowski <krystiann.nowakowski@gmail.com>
2022-04-18 22:33:36 +0200

    Add notebook

--- Etap2/start.py
+++ Etap2/start.py
@@ -2,7 +2,7 @@ import os
 
 #Czarne xception
 #os.system("main.py black_background_data results/black_background_data_xception_0 xception 0")
-#os.system("main.py black_background_data results/black_background_data_xception xception 1")
+os.system("main.py black_background_data results/xD xception 1")
 # os.system("main.py black_background_data results/black_background_data_xception_2 xception 2")
 
 #Czarne vgg16
@@ -16,8 +16,8 @@ import os
 # os.system("main.py black_background_data results/black_background_data_resnet50_2 resnet50 2")
 
 
-os.system("main.py white_background_data results/white_background_data_resnet50_lr_001 resnet50 1")
-os.system("main.py edge_data results/edge_data_resnet50_lr001 resnet50 1")
+#os.system("main.py white_background_data results/white_background_data_resnet50_lr_001 resnet50 1")
+#os.system("main.py edge_data results/edge_data_resnet50_lr001 resnet50 1")
 
 
 
