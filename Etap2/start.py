930a231cd8b19ba8f8dcbda5e27677a88dc0efe4
Krystian Nowakowski <krystiann.nowakowski@gmail.com>
2022-04-18 15:10:03 +0200

    Additional tests. Folder names refactoring

--- Etap2/start.py
+++ Etap2/start.py
@@ -1,2 +1,33 @@
 import os
-os.system("script2.py 1")
+
+#Czarne xception
+#os.system("main.py black_background_data results/black_background_data_xception_0 xception 0")
+#os.system("main.py black_background_data results/black_background_data_xception xception 1")
+# os.system("main.py black_background_data results/black_background_data_xception_2 xception 2")
+
+#Czarne vgg16
+# os.system("main.py black_background_data results/black_background_data_vgg16_0 vgg16 0")
+#os.system("main.py black_background_data results/black_background_data_vgg16 vgg16 1")
+# os.system("main.py black_background_data results/black_background_data_vgg16_2 vgg16 2")
+
+#Czarne resnet
+# os.system("main.py black_background_data results/black_background_data_resnet50_0 resnet50 0")
+#os.system("main.py black_background_data results/black_background_data_resnet50_1_lr001 resnet50 1")
+# os.system("main.py black_background_data results/black_background_data_resnet50_2 resnet50 2")
+
+
+os.system("main.py white_background_data results/white_background_data_resnet50_lr_001 resnet50 1")
+os.system("main.py edge_data results/edge_data_resnet50_lr001 resnet50 1")
+
+
+
+
+
+
+#os.system("main.py white_background_data results/white_background_data_xception xception 1")
+#os.system("main.py white_background_data results/white_background_data_vgg16 vgg16 1")
+#os.system("main.py white_background_data results/white_background_data_resnet50 resnet50 1")
+
+#os.system("main.py edge_data results/edge_data_xception xception 1")
+#os.system("main.py edge_data results/edge_data_vgg16 vgg16 1")
+#os.system("main.py edge_data results/edge_data_resnet50 resnet50 1")
